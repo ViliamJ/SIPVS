d31a00df589463c4c89d2b7e8026d0fc31af4480
SimonDrienik <sdrienik@gmail.com>
2022-10-03 12:24:40 +0200

    Finish commit without NameSpace

--- formular_web/xml_generator.py
+++ formular_web/xml_generator.py
@@ -10,9 +10,7 @@ def generateXML(car_formset: formset_factory(CarForm), file_name):
     root.appendChild(registration)
 
     # USER
-    #user = root.createElement('user')
-    user= root.createElementNS('http://hello.world/ns', 'ex:user')
-    user.setAttribute("xmlns:ex", "http://hello.world/ns")
+    user = root.createElement('user')
     registration.appendChild(user)
 
     first_name = root.createElement('first_name')
